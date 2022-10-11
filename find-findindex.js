4daf70ec8571d1a0b5affff1815dcc19d05b8007
alexm <alex-m@outlook.com>
2022-10-11 14:51:16 -0700

    Added logic for findUserByUsername(); broke up long comment lines

--- find-findindex.js
+++ find-findindex.js
@@ -2,8 +2,10 @@
 // 10/11/22
 // Unit 8.2.8: find, findIndex Exercise
 
-/* 
-Write a function called `findUserByUsername` which accepts an array of objects, each with a key of username, and a string. The function should return the first object with the key of username that matches the string passed to the function. If the object is not found, return undefined. 
+/*
+Write a function called `findUserByUsername` which accepts an array of objects, each with a key of
+username, and a string. The function should return the first object with the key of username that
+matches the string passed to the function. If the object is not found, return undefined.
 
 const users = [
   {username: 'mlewis'},
@@ -15,11 +17,17 @@ findUserByUsername(users, 'mlewis') // {username: 'mlewis'}
 findUserByUsername(users, 'taco') // undefined
 */
 
+function findUserByUsername(usersArray, username) {
 
-function findUserByUsername(usersArray, username) {}
+    return usersArray.find(function(object, index, array) {
+        return object["username"] === username;
+    })
+}
 
 /*
-Write a function called `removeUser` which accepts an array of objects, each with a key of username, and a string. The function should remove the object from the array. If the object is not found, return undefined. 
+Write a function called `removeUser` which accepts an array of objects, each with a key of
+username, and a string. The function should remove the object from the array. If the object is not
+found, return undefined.
 
 const users = [
   {username: 'mlewis'},
